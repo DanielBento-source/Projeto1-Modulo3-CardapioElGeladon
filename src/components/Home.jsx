49d8a79d7a1b3ec9b6a01c954f96c8fcdf5a3a89
DanielBento <dsbld2@gmail.com>
2022-04-26 13:38:11 -0300

    feat: adicionando estilização e implementando o primeiro use state

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,27 @@
 import './Home.css'
+import PaletaLista from './PaletaLista';
+import sacola from '../assets/icons/sacola.svg'
+import logo from '../assets/logo.svg'
 
 function Home() {
-    return <div className="Home">Este é o componente Home</div>;
+    return <div className="Home">
+        <div className="Home__header Header">
+		<div className="row">
+			<div className="Header__logo Logo">
+				<img src={logo} width="70px" alt="Logo El Geladon" className="Logo__icone" />
+				<span className="Logo__titulo"> El Geladon </span>
+			</div>
+			<div className="Header__opcoes Opcoes">
+				<div className="Opcoes__sacola Sacola">
+					<img src={sacola} width="40px" className="Sacola__icone" alt="Sacola de compras" />
+				</div>
+			</div>
+		</div>
+	</div>
+        <div className="Home__container">
+            <PaletaLista />
+        </div>
+    </div>;
   }
   
   export default Home;
